61fb3b1a6b914b6a82f8f54112cee942820cf0ab
Hasnain <Hasnainrashid1234@gmail.com>
2024-10-03 13:46:06 +0800

    More cahnges

--- quasar.conf.js
+++ quasar.conf.js
@@ -1,21 +1,28 @@
 /* eslint-env node */
 const ESLintPlugin = require('eslint-webpack-plugin');
 
-module.exports = function (ctx) { // ctx will help in accessing dev/prod mode settings
+module.exports = function (ctx) {
   return {
     supportTS: false,
-    boot: ['i18n', 'axios'],
+    boot: [
+      'i18n',
+      'axios',
+      'global-components',
+      'global-mixin',
+      'google-maps',
+      'gtm',
+      'vue-clipboard',
+      'lightbox',
+      'underscore',
+      'vue-notifications',
+    ],
     css: [],
     extras: ['roboto-font', 'material-icons'],
 
     build: {
-      vueRouterMode: 'hash', // 'hash' or 'history'
-
-      // Add the following settings:
-       // true when in production mode
-
-      mode: { spa: true }, // Specify SPA mode
-      modeName: 'spa', // Give the mode a name
+      rtl: false,
+      scopeHoisting: true,
+      vueRouterMode: 'history',
 
       target: {}, // Customize target if needed (e.g., web)
       targetName: undefined, // Define target name if necessary
@@ -25,16 +32,57 @@ module.exports = function (ctx) { // ctx will help in accessing dev/prod mode se
       // Use debug option
       debug: ctx.debug || false, // true if in debug mode
 
-      chainWebpack (chain) {
+      chainWebpack(chain) {
         chain.plugin('eslint-webpack-plugin')
           .use(ESLintPlugin, [{ extensions: ['js', 'vue'] }]);
-      }
+      },
     },
+    env: ctx.dev
+      ? {
+          APP_VERSION: "'1.9.43'",
+          PETBACKER_LOCAL: "'https://pbapidev.forwen.com/v5'",
+          PETBACKER_LOCAL_A: "'http://192.168.0.202/yii2-api/api/web/v5'",
+          PETBACKER_LOCAL_B: "'http://192.168.0.68/yii2-api/api/web/v5'",
+          PETBACKER_API_V5: "'https://api.petbacker.com/v5'",
+          PETBACKER_API_STAGING_V5: "'https://pbapistaging.forwen.com/v5'",
+          PETBACKER_API_STAGING_DEV: "'https://pbapidev.forwen.com/v5'",
+          FB_APP_KEY: "'1966773486968223'",
+          GOOGLE_MAPS_KEY: "'AIzaSyD8T0A1xZTGF1I7lcqxg8TNH5MrmalNZsI'",
+          VUE_ROUTER_MODE: "'history'",
+          GOOGLE_AUTH2_CLIENT_ID: "'589322981525-5de6v8dat603v01icvu4isipm5tj7qsq.apps.googleusercontent.com'",
+          GOOGLE_AUTH2_IOS_ID: "'589322981525-ucln80tpbv0cgatou0ptb48qccif4mb8.apps.googleusercontent.com'",
+          GOOGLE_AUTH2_ANDROID_ID: "'589322981525-8jmotd4ss8lu5mepa4e7tf3nfjuhu9qq.apps.googleusercontent.com'",
+          TAZAPAY_API_KEY: "'2G1KWV1P5JFOX5F4G96V'",
+          TAZAPAY_SIGNATURE_SECRET: "'sandbox_TPc4kpIyPLr9Q0t19RQWmCiZNbAGNtTfAyHEPzGBBm6qqLWynV2gSBWWwTlDQTh4dJyhfug9I74oxl7fyfjqTTUAi9pErzTwF9gyIFUPbeJZKMRWgYNhbeyjaRiLfEAU'",
+          TAZAPAY_API_URL: "'https://api-sandbox.tazapay.com/v1/'",
+          IOS_APP_ID: "'1168037472'",
+          ANDROID_APP_ID: "'com.petbacker.android'",
+        }
+      : {
+          APP_VERSION: "'1.9.43'",
+          PETBACKER_LOCAL: "'https://pbapidev.forwen.com/v5'",
+          PETBACKER_LOCAL_A: "'http://192.168.0.202/yii2-api/api/web/v5'",
+          PETBACKER_LOCAL_B: "'http://192.168.0.68/yii2-api/api/web/v5'",
+          PETBACKER_API_V5: "'https://api.petbacker.com/v5'",
+          PETBACKER_API_STAGING_V5: "'https://pbapistaging.forwen.com/v5'",
+          PETBACKER_API_STAGING_DEV: "'https://pbapidev.forwen.com/v5'",
+          FB_APP_KEY: "'1441906625823904'",
+          GOOGLE_MAPS_KEY: "'AIzaSyD8T0A1xZTGF1I7lcqxg8TNH5MrmalNZsI'",
+          VUE_ROUTER_MODE: "'history'",
+          GOOGLE_AUTH2_CLIENT_ID: "'589322981525-8j6bslqjhooekrjelq9uv09dd8n8cugn.apps.googleusercontent.com'",
+          GOOGLE_AUTH2_IOS_ID: "'589322981525-ucln80tpbv0cgatou0ptb48qccif4mb8.apps.googleusercontent.com'",
+          GOOGLE_AUTH2_ANDROID_ID: "'589322981525-8jmotd4ss8lu5mepa4e7tf3nfjuhu9qq.apps.googleusercontent.com'",
+          TAZAPAY_API_KEY: "'2G1KWV1P5JFOX5F4G96V'",
+          TAZAPAY_SIGNATURE_SECRET: "'sandbox_TPc4kpIyPLr9Q0t19RQWmCiZNbAGNtTfAyHEPzGBBm6qqLWynV2gSBWWwTlDQTh4dJyhfug9I74oxl7fyfjqTTUAi9pErzTwF9gyIFUPbeJZKMRWgYNhbeyjaRiLfEAU'",
+          TAZAPAY_API_URL: "'https://api-sandbox.tazapay.com/v1/'",
+          IOS_APP_ID: "'1168037472'",
+          ANDROID_APP_ID: "'com.petbacker.android'",
+        },
 
     devServer: {
       https: false,
       port: 8080,
-      open: true // opens browser window automatically
+      open: true, // opens browser window automatically
     },
 
     framework: {
@@ -42,7 +90,7 @@ module.exports = function (ctx) { // ctx will help in accessing dev/prod mode se
       lang: 'en-us',
       config: {},
       importStrategy: 'auto',
-      plugins: []
+      plugins: [],
     },
 
     animations: [],
@@ -60,34 +108,34 @@ module.exports = function (ctx) { // ctx will help in accessing dev/prod mode se
           {
             src: 'icons/icon-128x128.png',
             sizes: '128x128',
-            type: 'image/png'
+            type: 'image/png',
           },
           {
             src: 'icons/icon-192x192.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
           },
           {
             src: 'icons/icon-256x256.png',
             sizes: '256x256',
-            type: 'image/png'
+            type: 'image/png',
           },
           {
             src: 'icons/icon-384x384.png',
             sizes: '384x384',
-            type: 'image/png'
+            type: 'image/png',
           },
           {
             src: 'icons/icon-512x512.png',
             sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
+            type: 'image/png',
+          },
+        ],
+      },
     },
 
     capacitor: {
-      hideSplashscreen: false
+      hideSplashscreen: false,
     },
 
     electron: {
@@ -96,12 +144,12 @@ module.exports = function (ctx) { // ctx will help in accessing dev/prod mode se
         // Configure packager if necessary
       },
       builder: {
-        appId: 'petbacker'
+        appId: 'petbacker',
       },
       nodeIntegration: true,
-      extendWebpack (/* cfg */) {
+      extendWebpack(/* cfg */) {
         // Extend electron Webpack config here if necessary
-      }
-    }
+      },
+    },
   };
 };
